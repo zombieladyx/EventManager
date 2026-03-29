ca14be55974157925d3cf99ca349bb57d103e4f9
Kasia <kwysia@gmail.com>
2026-03-29 11:50:03 +0200

    Register, Login, Logout (2/4 video)

---
+++ EventManager/Components/Middleware/AuthMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Collections.Concurrent;
+using System.Security.Claims;
+
+namespace EventManager.Components.Middleware
+{
+    public class AuthMiddleware
+    {
+        private readonly RequestDelegate next;
+
+        public static IDictionary<Guid, ClaimsPrincipal> Logins {  get; private set; } = new ConcurrentDictionary<Guid, ClaimsPrincipal>();
+
+        public AuthMiddleware(RequestDelegate next)
+        {
+            this.next = next ?? throw new ArgumentNullException(nameof(next));
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if(context.Request.Path == "/login" && context.Request.Query.ContainsKey("key"))
+            {
+                var key = Guid.Parse(context.Request.Query["key"]);
+                var claim = Logins[key];
+                await context.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, claim);
+                context.Response.Redirect("/");
+            }
+
+            else
+            {
+                await next(context);
+            }
+        }
+    }
+}
---
+++ EventManager/Program.cs
@@ -0,0 +1,42 @@
+using EventManager.Components;
+using EventManager.Components.Middleware;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/login";
+        options.AccessDeniedPath = "/accessdenied";
+    });
+
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.UseMiddleware<AuthMiddleware>();
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
